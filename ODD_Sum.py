c03b33aba5bf65768cfbf346452c148cc825e185
Sagar449102 <96296233+Sagar449102@users.noreply.github.com>
2022-11-01 22:10:03 +0530

    import math n=int(input()) a=list(map(int,input().split())) res=0 res1=0 for i in range(len(a)):     if a[i]%2==0:         res=res+a[i]     else:         res1=res1+a[i] print(res1)

---
+++ ODD_Sum.py
@@ -0,0 +1,12 @@
+import math
+n=int(input())
+a=list(map(int,input().split()))
+res=0
+res1=0
+for i in range(len(a)):
+    if a[i]%2==0:
+        res=res+a[i]
+    else:
+        res1=res1+a[i]
+print(res1)
+
