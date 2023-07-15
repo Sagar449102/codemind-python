5f10d79b42c6633c270659c78e488169157cd1f2
Sagar449102 <96296233+Sagar449102@users.noreply.github.com>
2023-07-15 15:44:39 +0530

    n=input() s=n.split() a="aeiouAEIOU" c=0 for i in s:     if i[0] in a and i[-1] not in a:         c+=1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,11 +1,8 @@
-a=input()
-s=a.split()
-v="aeiouAEIOU"
-c="bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
-k=0
+n=input()
+s=n.split()
+a="aeiouAEIOU"
+c=0
 for i in s:
-    m=i[0]
-    e=i[len(i)-1]
-    if m in v and e in c:
-        k+=1
-print(k)
+    if i[0] in a and i[-1] not in a:
+        c+=1
+print(c)
